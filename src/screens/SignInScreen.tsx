c4be779ec51ab13990dcc47e419b27e9a1b0d87c
Elijah Wood <elijah@frodes.io>
2023-03-18 19:42:59 +0200

    Added react-hook-form to sign in

--- src/screens/SignInScreen.tsx
+++ src/screens/SignInScreen.tsx
@@ -4,6 +4,7 @@ import { HStack, KeyboardAvoidingView, VStack } from 'native-base'
 import styled from 'styled-components/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import PhoneInput from 'react-native-phone-number-input'
+import { useForm, Controller } from "react-hook-form"
 
 import { RootStackParamList } from '../../App'
 import DefaultButton from '../components/DefaultButton'
@@ -17,6 +18,13 @@ type Props = {
 }
 
 const SignInScreen: React.FC<Props> = ({ navigation }) => {
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      phone: '',
+    }
+  })
+
+  const onSubmit = data => console.log(data)
 
     return (
         <ContainerView>
@@ -24,11 +32,23 @@ const SignInScreen: React.FC<Props> = ({ navigation }) => {
                 <VStack space={5} justifyContent={'center'} style={{ height: '100%', marginHorizontal: 10, marginTop: 10 }}>
                     <TitleText>Please enter your phone number.</TitleText>
                     <SubtitleText>Enter your phone number.</SubtitleText>
-                    <PhoneInput
-                        containerStyle={{ width: '100%' }}
-                        defaultCode="US"
-                        layout="second"
-                        autoFocus
+                    {errors.phone && <ErrorText>This is required.</ErrorText>}
+                    <Controller
+                      control={control}
+                      rules={{
+                      required: true,
+                      }}
+                      render={({ field: { onChange, value } }) => (
+                        <PhoneInput
+                          containerStyle={{ width: '100%' }}
+                          value={value}
+                          onChangeText={onChange}
+                          defaultCode="US"
+                          layout="second"
+                          autoFocus
+                        />
+                      )}
+                      name="phone"
                     />
                     <HStack>
                         <TOSTextWrapper>
@@ -36,9 +56,7 @@ const SignInScreen: React.FC<Props> = ({ navigation }) => {
                             <TextButton label="Terms & Conditions." onPress={onTOS} color='grey' fontSize={12}/>
                         </TOSTextWrapper>
                     </HStack>
-                    <DefaultButton label='Continue' disabled={true} onPress={() => {
-                        
-                    }}/>
+                    <DefaultButton label='Continue' disabled={Object.keys(errors).length === 0 ? false : true} onPress={handleSubmit(onSubmit)}/>
                 </VStack>
             </KeyboardAvoidingView>
         </ContainerView>
@@ -70,7 +88,7 @@ const TOSTextWrapper = styled.View`
 `
 
 const ErrorText = styled.Text`
-  font-size: 12px;5
+  font-size: 12px;
   color: red;
 `
 
