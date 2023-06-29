0eb5e2275ed67517b0370831fd476304af798b80
Aleksei Ustinov <alvustinov@gmail.com>
2023-06-29 15:14:54 +0300

    Added contacts selection

---
+++ src/components/ContactCell.tsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import { Avatar, Divider, HStack, Spacer, VStack } from 'native-base'
+import Icon from 'react-native-vector-icons/Ionicons';
+import { GestureResponderEvent, TouchableOpacity } from 'react-native'
+import { getInitials } from '../utils/GetStringInitials'
+import { Contact } from 'expo-contacts'
+
+export interface SelectableContact extends Contact {
+	selected: boolean
+}
+
+type ContactCellProps = {
+	contact: SelectableContact
+	onPress: (e: GestureResponderEvent) => void
+}
+
+const ContactCell = (props: ContactCellProps) => {
+	const { contact, onPress } = props
+	const phoneNumber = contact.phoneNumbers?.[0].number ?? null
+	const email = contact.emails?.[0].email ?? null
+	const { selected } = contact
+
+	return <ContactCellContainerView>
+		<TouchableOpacity onPress={onPress}>
+			<VStack space={5}>
+				<HStack style={{alignItems: 'center'}}>
+					<Avatar>
+            {getInitials(contact.name)}
+          </Avatar>
+          <ContactCellContentView>
+          	<ContactTitleText>
+          		{contact.name}
+          	</ContactTitleText>
+          	
+          	{phoneNumber ?
+	          	<ContactFieldText>
+	          		{phoneNumber}
+	          	</ContactFieldText> :
+	          	null
+	          }
+	          
+	          {email ? 
+		          <ContactFieldText>
+		          	{email}
+		          </ContactFieldText> :
+		          null
+		        }
+          </ContactCellContentView>
+          
+          <Spacer />
+          
+          {selected ?
+          	<Icon name='checkmark-circle' color={'#0046FF'} size={30} /> :
+          	null
+          }
+				</HStack>
+				<Divider />
+			</VStack>
+		</TouchableOpacity>
+	</ContactCellContainerView>
+};
+
+const ContactCellContainerView = styled.View`
+	width: 100%;
+  padding-left: 16px;
+  padding-right: 18px;
+  padding-top: 16px;
+`;
+
+const ContactCellContentView = styled.View`
+	padding-horizontal: 12px;
+`
+
+const ContactTitleText = styled.Text`
+	font-size: 15px;
+	font-weight: bold;
+	line-height: 22px;
+`
+
+const ContactFieldText = styled.Text`
+	font-size: 15px;
+	line-height: 18px;
+`
+
+export default ContactCell
--- src/screens/ImportCustomersScreen.tsx
+++ src/screens/ImportCustomersScreen.tsx
@@ -1,16 +1,90 @@
-import { StackNavigationProp } from '@react-navigation/stack'
-import React from 'react'
-import { View } from 'react-native'
-import { RootStackParamList } from '../../App'
-
-type NavigationProp = StackNavigationProp<RootStackParamList, 'ImportCustomersScreen'>
-
-type Props = {
-  navigation: NavigationProp
-}
+import React, { useEffect, useState } from 'react'
+import styled from 'styled-components/native'
+import { SearchBar } from '@rneui/themed'
+import { requestPermissionsAsync, getContactsAsync, PermissionStatus } from 'expo-contacts'
+import { FlatList } from 'react-native'
+import UseCustomers from '../api/UseCustomers'
+import ContactCell, { SelectableContact } from '../components/ContactCell'
+import DefaultButton from '../components/DefaultButton'
 
 const ImportCustomersScreen = () => {
-	return <View />
+	const useCustomers = UseCustomers()
+	const [search, setSearch] = useState('');
+	const [contacts, setContacts] = useState<SelectableContact[] | null>(null);
+
+	const syncContacts = async () => {
+		if (!useCustomers.data) {
+			await useCustomers.refetch();
+		}
+		// const existingCustomers = useCustomers.data;
+		const { status } = await requestPermissionsAsync();
+    if (status !== PermissionStatus.GRANTED) {
+      // TODO show message about contacts permissions
+      return;
+    }
+    const { data } = await getContactsAsync();
+    setContacts(data.map(contact => ({ ...contact, selected: false })));
+	}
+
+	const toggleContact = (selectedContact: SelectableContact) => {
+		setContacts(contacts => contacts.map(contact => {
+			if (contact.id === selectedContact.id) {
+				return { ...contact, selected: !contact.selected }
+			}
+			return contact
+		}))
+	};
+
+	useEffect(() => {
+		syncContacts()
+	}, []);
+
+ 	if (useCustomers.isLoading || !contacts) {
+    return (
+      <ContainerView>
+          <PaddedActivityIndicator/>
+      </ContainerView>
+    )
+  }
+
+  const selectedContacts = contacts.filter(contact => contact.selected);
+
+	return <ContainerView>
+		<SearchBar
+      platform="ios"
+      placeholder="Search by name, email, or phone"
+      onChangeText={setSearch}
+      value={search}
+    />
+    <FlatList
+    	data={contacts}
+    	keyExtractor={item => item.id}
+    	renderItem={({ item }) => <ContactCell contact={item} onPress={() => toggleContact(item)} />}
+    	contentContainerStyle={{flexGrow: 1}}
+    	keyboardDismissMode="interactive"
+    />
+    <ButtonContainerView>
+    	<DefaultButton
+    		label={selectedContacts.length > 0 ? `Add ${selectedContacts.length} contacts` : `Add contacts`}
+    		disabled={selectedContacts.length === 0}
+  		/>
+    </ButtonContainerView>
+	</ContainerView>
 }
+
+const PaddedActivityIndicator = styled.ActivityIndicator`
+  padding: 12px;
+`
+
+const ContainerView = styled.View`
+  height: 100%;
+  width: 100%;
+  background-color: white;
+`
+
+const ButtonContainerView = styled.View`
+	padding-horizontal: 16px;
+	padding-vertical: 15px;
+`
 
 export default ImportCustomersScreen
