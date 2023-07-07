95ff76c2840c70e9a0c8ed423660ca557529f6f2
Aleksei Ustinov <alvustinov@gmail.com>
2023-07-07 09:33:02 +0300

    Fixed API request

--- src/api/UseUploadMembers.tsx
+++ src/api/UseUploadMembers.tsx
@@ -1,8 +1,14 @@
 import { UseMutationResult, useMutation } from 'react-query'
 import { API } from './API'
-import { Member } from './UseCustomers'
 
-type UploadMembersInput = Member[]
+export type UploadMemberContact = {
+    phone?: string
+    email?: string
+    first_name?: string
+    last_name?: string
+}
+
+type UploadMembersInput = UploadMemberContact[]
 
 type UploadMembersResponse = {
     ok: boolean
--- src/screens/ImportTeamMembersScreen.tsx
+++ src/screens/ImportTeamMembersScreen.tsx
@@ -1,9 +1,9 @@
-import { RouteProp } from '@react-navigation/native';
+import { RouteProp, useNavigation } from '@react-navigation/native';
 import { Contact } from 'expo-contacts';
 import React, { useEffect, useMemo, useState } from 'react'
+import { Alert } from 'react-native';
 import { RootStackParamList } from '../../App';
-import { Member } from '../api/UseCustomers';
-import UseUploadMembers from '../api/UseUploadMembers';
+import UseUploadMembers, { UploadMemberContact } from '../api/UseUploadMembers';
 import { SelectableContact } from '../components/ContactCell';
 import ImportContactList from '../components/ImportContactList';
 import { formatPhoneNumber } from '../utils/FormatPhoneNumber';
@@ -11,71 +11,80 @@ import { formatPhoneNumber } from '../utils/FormatPhoneNumber';
 type ImportTeamMembersScreenRouteProp = RouteProp<RootStackParamList, 'ImportTeamMembersScreen'>
 
 type ImportTeamMembersScreenProps = {
-	route: ImportTeamMembersScreenRouteProp
+  route: ImportTeamMembersScreenRouteProp
 }
 
 const ImportTeamMembersScreen = (props: ImportTeamMembersScreenProps) => {
-	const useUploadMembers = UseUploadMembers()
-	const members = props.route?.params?.members ?? []
-	const phoneNumbers: string[] = useMemo(() => {
-		return members.map(member => member.User.phone)
-	}, [members])
+  const useUploadMembers = UseUploadMembers()
+  const navigation = useNavigation()
+  const members = props.route?.params?.members ?? []
+  const phoneNumbers: string[] = useMemo(() => {
+    return members.map(member => member.User.phone)
+  }, [members])
 
-	const [isUploading, setUploading] = useState(false)
+  const [isUploading, setUploading] = useState(false)
 
-	useEffect(() => {
-		switch (useUploadMembers.status) {
-		case 'success':
-			setUploading(false)
-    case 'error':
-    	setUploading(false)
-		default:
-			break
-		}
-	}, [useUploadMembers.status])
+  useEffect(() => {
+    switch (useUploadMembers.status) {
+      case 'success':
+        setUploading(false)
+        console.log(useUploadMembers.data);
+        Alert.alert(
+          'Contacts who don’t already have Serv accounts will be invited.',
+          'Once they accept the invitation, you’ll be able to message them here.',
+          [
+            {text: 'OK', onPress: () => navigation.goBack()}
+          ]
+        )
+      case 'error':
+        setUploading(false)
+      default:
+        break
+    }
+  }, [useUploadMembers.status])
 
-	const handleImport = (contacts: SelectableContact[]) => {
-		setUploading(true)
-		const members = contacts.map(contact => {
-			let member = {
-				first_name: contact.firstName,
-				last_name: contact.lastName,
-				email: contact.emails?.[0].email,
-				phone: formatPhoneNumber(contact?.phoneNumbers?.[0].number)
-			}
-			return member
-		});
-		useUploadMembers.mutate(members)
-		setUploading(false)
-	}
+  const handleImport = (contacts: SelectableContact[]) => {
+    setUploading(true)
+    const members = contacts.map(contact => {
+      let member = {
+        first_name: contact.firstName,
+        last_name: contact.lastName,
+        email: contact.emails?.[0].email,
+        phone: formatPhoneNumber(contact?.phoneNumbers?.[0].number)
+      } as UploadMemberContact
+      return member
+    });
+    useUploadMembers.mutate(members)
+    setUploading(false)
+  }
 
-	const filterContact = (contact: Contact) => {
-		if (!contact.phoneNumbers || contact.phoneNumbers?.length === 0) {
-			return false
-		}
-		if (phoneNumbers.indexOf(formatPhoneNumber(contact.phoneNumbers?.[0].number)) !== -1) {
-			return false
-		}
-		return true;
-	}
+  const filterContact = (contact: Contact) => {
+    if (!contact.phoneNumbers || contact.phoneNumbers?.length === 0) {
+      return false
+    }
+    if (phoneNumbers.indexOf(formatPhoneNumber(contact.phoneNumbers?.[0].number)) !== -1) {
+      return false
+    }
+    return true;
+  }
 
-	return <ImportContactList
-		filterContact={filterContact}
-		onImport={handleImport}
-		importButtonLabel={getImportButtonLabel}
-		isUploading={isUploading}
-	/>;
+  return <ImportContactList
+    filterContact={filterContact}
+    onImport={handleImport}
+    importButtonLabel={getImportButtonLabel}
+    isUploading={isUploading}
+  />;
 }
 
 
 const getImportButtonLabel = (count: number): string => {
-	if (count === 1) {
-		return `Add ${count} Team Member`
-	}
-	if (count > 1) {
-		return `Add ${count} Team Members`
-	}
-	return 'Add Team Members'
+  if (count === 1) {
+    return `Add ${count} Team Member`
+  }
+  if (count > 1) {
+    return `Add ${count} Team Members`
+  }
+  return 'Add Team Members'
 }
 
 
